586d02517681b80566b790f35c447dc24b271257
rifath33 <hrifath20@stuy.edu>
2021-01-07 14:24:41 -0500

    made the get and set methods for all three instance variables

--- Node.java
+++ Node.java
@@ -1,6 +1,38 @@
 public class Node{
- public Node(String value){/*create a constructor*/}
+    
+ public Node(String value){
+     
+ }
  private String data;
  private Node next,prev;
+ 
+ public String getData(){
+     return data;
+ }
+ 
+ public boolean setData(String data){
+     this.data = data;
+     return true;
+ }
+ 
+ public Node getNext(){
+     return next;
+ }
+ 
+ public boolean setNext(Node next){
+     this.next = next;
+     return true;
+ }
+ 
+  public Node getPrev(){
+     return prev;
+ }
+ 
+ public boolean setPrev(Node prev){
+     this.prev = prev;
+     return true;
+ }
+ 
  //write get/set methods for all three instance variables.
+ 
 }
