da1bbff893b2253270be9cad1225679d4fd2ed01
rifath33 <hrifath20@stuy.edu>
2021-01-11 15:50:35 -0500

    fixed compling issues

--- MyLinkedList.java
+++ MyLinkedList.java
@@ -22,13 +22,15 @@ public class MyLinkedList{
         end = end.getNext();
     }
     
+    return true;
+    
  }
  
  public void add(int index, String value){
     
     Node addition = new Node(value);
     
-    if(size()==0 && index>0)
+    if(this.size()==0 && index>0)
     throw new IndexOutOfBoundsException("Your index can't be more than 0 if the List is empty...");
     
     if(index == 0 && size() == 0){
@@ -43,13 +45,13 @@ public class MyLinkedList{
         size++;
     }
     
-    if(index == list.size()-1 && size() > 0){
+    if(index == this.size()-1 && size() > 0){
         end.setNext(addition);
         end = end.getNext();
         size++;
     }
     
-    if(index > 0 && index < list.size()-1 && size()){
+    if(index > 0 && index < this.size()-1 && this.size()>0){
         getNode(index).getPrev().setNext(addition);
         getNode(index).getNext().setPrev(addition);
         size++;        
@@ -58,27 +60,35 @@ public class MyLinkedList{
  }
  
  private Node getNode(int index){
+    Node currentNode = start;
      
     if(index ==0) 
     return start;
-    
-    if(n == size()-1)
-    return end;
-    
-    if(size>0){
-        Node currentNode = start;
-        for(int i = 0; i < index; i++){
-            currentNode = currentNode.getNext();
+    else{
+        
+        if(index == this.size()-1)
+        return end;
+        else{
+            
+            if(this.size()>0){
+            
+            for(int i = 0; i < index; i++){
+                currentNode = currentNode.getNext();
+            }
+            
+            }
+            
+            return currentNode;
+            
         }
+        
     }
-    
-    return currentNode;
     
  }
 
  
  public String get(int index){
-    return getNode(index).geteData();
+    return getNode(index).getData();
  }
  
  public String set(int index, String value){
