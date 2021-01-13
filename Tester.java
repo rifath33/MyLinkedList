cdd798a10e2ab37be05c78364028ea1570d64a1b
rifath33 <hrifath20@stuy.edu>
2021-01-13 04:20:58 -0500

    just used the konstantinovich sample

--- Tester.java
+++ Tester.java
@@ -1,18 +1,24 @@
 public class Tester{
   public static void main (String[] args){
     
-    MyLinkedList rifath = new MyLinkedList();
-    rifath.add("cargo");
-    rifath.add("thing");
-    rifath.add("hello");
-    rifath.add("hey");
-    rifath.add("wow");
-    rifath.add("heyyo");
-    rifath.add("unnus");
-    rifath.add("annus");
-    System.out.println(rifath.toString());
-    rifath.remove(-1);
-    System.out.println(rifath.toString());
+    MyLinkedList a = new MyLinkedList();
+    MyLinkedList b = new MyLinkedList();
+    for(int i = 0; i < 10; i++){
+      if(i < 5){
+        a.add(i+"");
+      }else{
+        b.add(i+"");
+      }
+    }
+    System.out.println();
+    System.out.println("A:"+a+a.size());
+    System.out.println("B:"+b+b.size());
+
+    a.extend(b);
+    System.out.println("A:"+a+a.size());
+    System.out.println("B:"+b+b.size());
+    System.out.println("A reversed:"+a.toStringReversed()+a.size());
+    System.out.println("B reversed:"+b.toStringReversed()+b.size());
     
   }
 }
